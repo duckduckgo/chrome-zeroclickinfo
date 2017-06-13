2d79b3aebb353378ac7cc52d679fc460e3e5b57a
jdorweiler <jason@duckduckgo.com>
2017-06-13 10:19:30 -0600

    add old option page

---
+++ js/version.js
@@ -0,0 +1,70 @@
+let version = (() => {
+    let name;
+    let oldOptionPage = "chrome-extension://" + chrome.runtime.id + "/html/option-old.html";
+
+    /*
+     * Rewrite the old options page to the new one
+     */
+    let optionPageRequestListener = ((req) => { 
+        if (version.name === "beta" && req.url && req.url === oldOptionPage) {
+            let newOptionPage = "chrome-extension://" + chrome.runtime.id + "/html/options.html";
+            return {redirectUrl: newOptionPage};
+        }
+    });
+
+    return {
+        update: (value) => {
+            if (value === "beta") {
+                version.betaStateOn();
+            }
+            else {
+                version.betaStateOff();
+            }
+            version.name = value;
+            settings.updateSetting('version', value);
+            return version.name;
+        },
+
+        betaStateOn: () => {
+            chrome.browserAction.setPopup({popup:'html/trackers.html'});
+            settings.updateSetting('extensionIsEnabled', true);
+            settings.updateSetting('httpsEverywhereEnabled', true);
+            settings.updateSetting('trackerBlockingEnabled', true);
+            settings.updateSetting('embeddedTweetsEnabled', false);
+        },
+
+        betaStateOff: () => {
+            chrome.browserAction.setPopup({popup:'html/popup.html'});
+            settings.updateSetting('extensionIsEnabled', false);
+            settings.updateSetting('httpsEverywhereEnabled', false);
+            settings.updateSetting('trackerBlockingEnabled', false);
+            settings.updateSetting('embeddedTweetsEnabled', true);
+        },
+
+        startup: () => {
+            // call out own update method to make sure all setting 
+            // are in the correct state
+            version.update(settings.getSetting('version'));
+            
+            // register listener to rewrite the options page request
+            chrome.webRequest.onBeforeRequest.addListener(optionPageRequestListener, {
+                urls: ['<all_urls>']
+            }, ['blocking']);
+
+            // serpVersion.js will will check the serp for an extension version to use
+            chrome.runtime.onMessage.addListener((message) => {
+                if (message && message.versionFlag) {
+                    console.log("Setting version to: ", message.versionFlag);
+                    version.update(message.versionFlag)
+                }
+            });
+        }
+    }
+})();
+
+/*
+ * On startup check the saved version in default settings
+ * Update the popup and privacy feature settings
+ */
+version.startup();
+
