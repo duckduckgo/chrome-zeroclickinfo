25153f0311c75ba3802c1f122a462701b4302b21
jdorweiler <jason@duckduckgo.com>
2017-06-28 17:34:09 -0600

    update attribute

--- js/serpVersion.js
+++ js/serpVersion.js
@@ -1,4 +1,4 @@
-var extensionVersionFlag = document.querySelector('html').getAttribute('data-chromeversion');
+var extensionVersionFlag = document.querySelector('html').getAttribute('data-extensionversion');
 
 if (extensionVersionFlag) {
     chrome.runtime.sendMessage({versionFlag: extensionVersionFlag});
