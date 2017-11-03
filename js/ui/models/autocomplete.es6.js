6452a433d623d29f47f211a08b2db26610ba2805
Lauren Garcia <lauren.garcia@gmail.com>
2017-11-02 20:21:38 -0700

    linter: globals

--- js/ui/models/autocomplete.es6.js
+++ js/ui/models/autocomplete.es6.js
@@ -4,22 +4,21 @@ function Autocomplete (attrs) {
     Parent.call(this, attrs)
 }
 
-
 Autocomplete.prototype = $.extend({},
-  Parent.prototype,
-  {
+    Parent.prototype,
+    {
 
-      modelName: 'autocomplete',
+        modelName: 'autocomplete',
 
-      fetchSuggestions: function (searchText) {
-          return new Promise((resolve, reject) => {
-              // TODO: ajax call here to ddg autocomplete service
-              // for now we'll just mock up an async xhr query result:
-              this.suggestions = [`${searchText} world`, `${searchText} united`, `${searchText} famfam`]
-              resolve()
-          })
-      }
-  }
+        fetchSuggestions: function (searchText) {
+            return new Promise((resolve, reject) => {
+                // TODO: ajax call here to ddg autocomplete service
+                // for now we'll just mock up an async xhr query result:
+                this.suggestions = [`${searchText} world`, `${searchText} united`, `${searchText} famfam`]
+                resolve()
+            })
+        }
+    }
 )
 
 
