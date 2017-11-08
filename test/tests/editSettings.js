b8f54e00cd4500c0b77c6ff88b72220a30a1fedf
jdorweiler <jason@duckduckgo.com>
2017-11-08 14:02:25 -0700

    settings categories

--- test/tests/editSettings.js
+++ test/tests/editSettings.js
@@ -1,17 +1,19 @@
 let bkg = chrome.extension.getBackgroundPage()
 const defaultSettings = bkg.settings.getDefaults()
 
-// map setting names to categories so we can build a nice table
+// Add settings here that a user might need to edit frequently. Any other settings not listed here will
+// go into the "Other" category and hidden by default
 let categories = {
-    'Block Lists and Settings': ['entityMap', 'entityList', 'easylists', 'blocking', 'generalEasylist', 'privacyEasylist' ],
-    'Whitelists': ['trackersWhitelist', 'trackersWhitelistTemporary', 'httpsWhitelist'],
-    "Other": []
+    'Whitelists': ['trackersWhitelist', 'trackersWhitelistTemporary', 'httpsWhitelist', 'HTTPSwhitelisted'],
+    'Block Lists': ['entityList', 'easylists', 'blocking', 'generalEasylist', 'privacyEasylist' ],
 }
 
 // build a regex string of categories to make filtering easier when we build the table
-let defaultCategories = ['Block Lists and Settings', 'Whitelists'].map((category) => categories[category].join('|')).join('|')
+let defaultCategories = Object.keys(categories).map((category) => categories[category].join('|')).join('|')
 
-function settingsByCategory(settings) {
+function buildCategories(settings) {
+    categories.Other = []
+
     for(let setting in settings) {
         if(!setting.match(defaultCategories)) {
             categories.Other.push(setting)
@@ -21,18 +23,21 @@ function settingsByCategory(settings) {
 }
 
 function buildTable (newSettings) {
-    let settings = settingsByCategory(newSettings || bkg.settings.getSetting())
+    let settings = newSettings || bkg.settings.getSetting()
+    buildCategories(settings)
     let elements = []
    
     // generate output table
-    let output = '<h2>Settings</h2><table><th>Name</th><th>Value</th>'
+    let output = '<h2>Settings</h2><table><th>Category</th><th>Name</th><th>Value</th>'
 
     for(let category in categories) {
-        for(let setting in categories[category]) {
+        output += `<tr class='${category}'><td class='category-name'>${category}</td><td></td><td></td></tr>`
+
+        categories[category].forEach((setting) => {
             let value = (typeof settings[setting] === 'object') ? JSON.stringify(settings[setting]) : settings[setting]
-            output += `<tr><td class='setting-name'>${setting}</td><td><input type='text' id=${setting} value=${value}></td></tr>`
+            output += `<tr class='${category}'><td class='category-name'></td><td class='setting-name'>${setting}</td><td><input type='text' id=${setting} value=${value}></td></tr>`
             elements.push(setting)
-        }
+        })
     }
 
     output += '</table>'
@@ -85,14 +90,20 @@ function loadSettingsFromUser () {
     }
 }
 
+function toggleOtherSettings () {
+    $('tr.Other').toggle()
+}
+
 $('#reset').on('click', resetSettings)
 $('#export').on('click', exportSettings)
 $('#load').on('click', loadSettingsFromUser)
 $('#reload').on('click', () => chrome.runtime.reload())
+$('#show-other').on('click', toggleOtherSettings)
 
 buildTable()
 
 $(document).ready(() => {
     $("tr:even").css("background-color", "#eeeeee");
     $("tr:odd").css("background-color", "#ffffff");
+    toggleOtherSettings()
 });
